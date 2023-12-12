1e051113f74065172e074ee706fabb854a33d006
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2023-12-12 14:20:41 +0200

    different variants and explanation

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ console.log(array); // Результат: [1, '2', 3, '5', 4, 6, '7']
 */
 
 function removeElement(array, item) {
+  /* ЦЕ БУВ ПЕРШИЙ ВАРІАНТ:
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      array.splice(i, 1);
+      i -= 1;
+    }
+}*/
+
   const firstIndex = 0;
   const lastIndex = array.length - 1;
 
@@ -21,12 +30,17 @@ function removeElement(array, item) {
       array.splice(currentIndex, 1);
     }
   }
+
   return array;
 }
 
 const array = [1, '2', 5, 5, 3, '5', 4, 5, 6, '7'];
 removeElement(array, 5);
 
-console.log(removeElement(array, 5));
+console.log(array);
 
-// const removeElement = (array, item) => array.filter(element => element !== item)
+/* 
+Якби не треба було змінюваити вхідний масив, то краще повернути новий масив через метод filter, з елментами, які не дорівнюють item
+
+const removeElement = (array, item) => array.filter(element => element !== item)
+*/
